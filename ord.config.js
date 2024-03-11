6ad8062e2cbbc7d9c73c3c13bd15c96cb5f026d2
Aaron <aarongkendall@gmail.com>
2024-03-11 12:00:32 +0000

    update on chain inscription ids for styles and js

--- ord.config.js
+++ ord.config.js
@@ -2,9 +2,11 @@ module.exports = {
   dependencies: {},
   img: {},
   styles: {
-    "styles.css": "ordi0",
+    "styles.css":
+      "83a19f63e4cc2ed5cba8ec24df7db27f3004d7b87db5d906685c5391ad88434fi0",
   },
   script: {
-    "main.js": "ordi0",
+    "main.js":
+      "52ddfa1e8f78cbe19ff82cd8a8e32cb3dfcbebc4f793b8f07c719cd3dd20a006i0",
   },
 };
